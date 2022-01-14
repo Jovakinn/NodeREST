239b76bd7382fae018093859924f1111b12c98cb
Jovakinn <max05012004@gmail.com>
2022-01-14 17:41:13 +0200

    add mongoose api

--- users/services/users.service.ts
+++ users/services/users.service.ts
@@ -6,20 +6,20 @@ class UserService implements CRUD {
         return UsersDao.addUser(resource);
     }
 
-    async deleteById(id: string): Promise<string> {
-        return UsersDao.removeUserById(id);
+    async deleteById(id: string): Promise<any> {
+        return UsersDao.deleteUserById(id);
     }
 
     async list(limit: number, page: number): Promise<any> {
-        return UsersDao.getUsers();
+        return UsersDao.getUsers(limit, page);
     }
 
     async patchById(id: string, resource: any): Promise<string> {
-        return UsersDao.patchUserById(id, resource);
+        return UsersDao.updateUserById(id, resource);
     }
 
     async putById(id: string, resource: any): Promise<string> {
-        return UsersDao.putUserById(id, resource);
+        return UsersDao.updateUserById(id, resource);
     }
 
     async readById(id: string): Promise<any> {
